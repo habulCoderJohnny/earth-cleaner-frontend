bd9022f7978fd5599e560a0da773374df2a3d72b
habulCoderJohnny <fokruljohnny@gmail.com>
2023-10-16 13:29:15 +0600

    name changes

--- src/components/reuseable/ReviewCard.tsx
+++ src/components/reuseable/ReviewCard.tsx
@@ -4,7 +4,7 @@ export default function ReviewCard() {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">Riyad Hossain</h2>
+        <h2 className="card-title">earthCleaner</h2>
         <p>
           I literally got amazed by the service the provide. Everything is very
           much neat and clear now.
