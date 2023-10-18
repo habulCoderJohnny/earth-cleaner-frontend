b0e5f948c640fb9a06c823fb6f7d97e829905c6c
habulCoderJohnny <fokruljohnny@gmail.com>
2023-10-19 00:21:32 +0600

    base API updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,13 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { tagTypes } from "../tagTypes";
 import { getFromLocalStorage } from "@/utils/local-storage";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" });
-
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    prepareHeaders: (headers, { getState }) => {
+    baseUrl: "https://earth-cleaner-backend.vercel.app/api/v1",
+    prepareHeaders: (headers) => {
       const accessToken = getFromLocalStorage("accessToken");
       if (accessToken) {
         headers.set("Authorization", accessToken);
